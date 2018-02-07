b2a991c05a84bde6cdae1aa246f3eadbb767aaca
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2018-02-08 03:31:11 +0700

    add TDD

--- __tests__/App.js
+++ __tests__/App.js
@@ -1,12 +1,60 @@
-import 'react-native';
-import React from 'react';
-import App from '../App';
+import 'react-native'
+import React from 'react'
+import App from '../App'
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer'
 
-it('renders correctly', () => {
-  const tree = renderer.create(
-    <App />
-  );
-});
+describe('<App />', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<App />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+  it('works, hopefully', () => {
+    expect(true).toEqual(true)
+  })
+  it('should have <Provider>', () => { })
+  it('should have <StackNavigator>', () => { })
+})
+
+describe('<Login />', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<Login />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+  it('should have <Chat /> tag', () => { })
+  it('should have login button', () => { })
+  it('should link to <Chat /> screen', () => { }) // simulate
+})
+
+describe('<Chat />', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<Chat />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+  it('should have header', () => { })
+  it('should have form input', () => { })
+  it('should can receive text input', () => { })
+  it('should can post message', () => { })
+  it('should can display new message', () => { })
+  it('should can display older messages', () => { })
+  it('should can display article options', () => { })
+  it('should can select article', () => { })
+  it('should can remove article options', () => { })
+})
+
+describe('<ReadingList />', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<ReadingList />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+  it('should have <ReadingItem /> screen', () => { })
+  it('should have <Overview /> screen', () => { })
+})
+
+describe('<Overview />', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<Overview />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
