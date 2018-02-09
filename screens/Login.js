e99fda4cf2ca32cd2eaf23a071a82d270e1a3a38
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2018-02-09 08:59:30 +0700

    success setup facebook sdk for react-native

--- screens/Login.js
+++ screens/Login.js
@@ -6,29 +6,76 @@ import {
   View,
   Button
 } from 'react-native'
+// import { LoginButton } from 'react-native-fbsdk'
+import { LoginButton, AccessToken, LoginManager } from 'react-native-fbsdk';
 
 class Login extends Component {
   static navigationOptions = {
     title: 'Login',
   }
 
+  onLoginOrRegister = () => {
+    LoginManager.logInWithReadPermissions(['public_profile', 'email'])
+    .then((result) => {
+      if (result.isCancelled) {
+        return Promise.reject(new Error('The user cancelled the request'));
+      }
+      // Retrieve the access token
+      return AccessToken.getCurrentAccessToken();
+    })
+    .then((data) => {
+      // Create a new Firebase credential with the token
+      const credential = firebase.auth.FacebookAuthProvider.credential(data.accessToken);
+      // Login with the credential
+      return firebase.auth().signInWithCredential(credential);
+    })
+    .then((user) => {
+      // If you need to do anything with the user, do it here
+      // The user will be logged in automatically by the
+      // `onAuthStateChanged` listener we set up in App.js earlier
+    })
+    .catch((error) => {
+      const { code, message } = error;
+      // For details of error codes, see the docs
+      // The message contains the default Firebase string
+      // representation of the error
+    });
+  }
+
   render() {
     const { navigate } = this.props.navigation
     return (
-      <View style={styles.container}>
-        <TouchableOpacity
-          style={[styles.button, styles.facebook]}
-          onPress={() =>
-            navigate('Home')}
-        >
-          <Text style={styles.textColor}>Login with Facebook</Text>
-        </TouchableOpacity>
+      // <View style={styles.container}>
+      //   <TouchableOpacity
+      //     style={[styles.button, styles.facebook]}
+      //     onPress={() =>
+      //       navigate('Home')}
+      //   >
+      //     <Text style={styles.textColor}>Login with Facebook</Text>
+      //   </TouchableOpacity>
 
-        <TouchableOpacity
-          style={[styles.button, styles.google]}
-        >
-          <Text>Login with Google</Text>
-        </TouchableOpacity>
+      //   <TouchableOpacity
+      //     style={[styles.button, styles.google]}
+      //   >
+      //     <Text>Login with Google</Text>
+      //   </TouchableOpacity>
+      // </View>
+      <View style={styles.container}>
+        <LoginButton
+          publishPermissions={['publish_actions']}
+          onLoginFinished={
+            (error, result) => {
+              if (error) {
+                alert('Login failed with error: ' + result.error);
+              } else if (result.isCancelled) {
+                alert('Login was cancelled');
+              } else {
+                alert('Login was successful with permissions: ' + result.grantedPermissions)
+                console.log('result -->', result)
+              }
+            }
+          }
+          onLogoutFinished={() => alert('User logged out')}/>
       </View>
     )
   }
